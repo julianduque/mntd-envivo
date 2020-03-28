d95ead5b5be91d8596e157411b025fb8d4d208cf
Julian Duque <julianduquej@gmail.com>
2020-03-28 17:00:12 -0400

    add sequelize support

---
+++ node/secrets-cli/models/user.js
@@ -0,0 +1,21 @@
+'use strict'
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    username: DataTypes.STRING,
+    password: DataTypes.STRING,
+    fullName: {
+      type: DataTypes.STRING
+    }
+  }, {
+    underscored: true,
+    tableName: 'users'
+  })
+  User.associate = function (models) {
+    User.hasMany(models.Secret, {
+      sourceKey: 'username',
+      foreignKey: 'username',
+      as: 'secrets'
+    })
+  }
+  return User
+}
