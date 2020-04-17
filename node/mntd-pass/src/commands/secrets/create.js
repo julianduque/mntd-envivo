79e120fdcec0ec6c665b8d44999bf8dd8b24fb49
Jorman <tille100@gmail.com>
2020-04-16 19:32:34 -0500

    Add Authenticate:hook to remove duplicate code.

--- node/mntd-pass/src/commands/secrets/create.js
+++ node/mntd-pass/src/commands/secrets/create.js
@@ -4,7 +4,6 @@ const { Command } = require('@oclif/command')
 const { CLIError } = require('@oclif/errors')
 const { cli } = require('cli-ux')
 const { Sequelize } = require('@mntd/db')
-const { AUTHENTICATED, isAuthenticated, authenticate } = require('@mntd/auth')
 const { secretServices } = require('@mntd/services')
 
 class SecretsCreateCommand extends Command {
@@ -12,16 +11,10 @@ class SecretsCreateCommand extends Command {
     try {
       const { args } = this.parse(SecretsCreateCommand)
       const { username, name } = args
-      let password = AUTHENTICATED
-      if (!await isAuthenticated(username)) {
-        password = await cli.prompt('Enter your password', { type: 'hide' })
-
-        const user = await authenticate(username, password)
-        if (!user) throw new CLIError('Invalid user or password')
-      }
+      await this.config.runHook('authenticate', { username })
 
       const value = await cli.prompt('Enter your secret', { type: 'mask' })
-      const secret = await secretServices.createSecret(username, password, name, value)
+      const secret = await secretServices.createSecret(username, name, value)
 
       this.log(`secret: ${secret.name} created for user '${username}'`)
     } catch (err) {
